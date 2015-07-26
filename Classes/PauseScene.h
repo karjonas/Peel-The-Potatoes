079fc3d18659e54066d42a807bdb97fe5b66577b
jonask <jonaskarlsson@fripost.org>
2015-07-26 17:31:10 +0200

    Add Pause Scene

---
+++ Classes/PauseScene.h
@@ -0,0 +1,30 @@
+#ifndef __PAUSE_SCENE_H__
+#define __PAUSE_SCENE_H__
+
+#include "cocos2d.h"
+
+class PauseScene : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(PauseScene);
+
+    void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+    void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+
+    cocos2d::Sprite* button_holder = nullptr;
+    cocos2d::Label* exit_label = nullptr;
+    cocos2d::Label* continue_label = nullptr;
+    cocos2d::Label* selected_label = nullptr;
+};
+
+#endif // __PAUSE_SCENE_H__
